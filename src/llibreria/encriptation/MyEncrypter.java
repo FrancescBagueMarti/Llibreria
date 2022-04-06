7103ce42f10d5a43d1d12cc46ad40f6c0d66a890
fbaguem2021 <fbaguem2021@cepnet.net>
2022-04-06 15:47:08 +0200

    Añadido de imports en MyEncrypter

--- src/llibreria/encriptation/MyEncrypter.java
+++ src/llibreria/encriptation/MyEncrypter.java
@@ -1,6 +1,17 @@
 package llibreria.encriptation;
-import java.security.SecureRandom;
 
+import java.security.SecureRandom;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 import org.mindrot.jbcrypt.BCrypt;
 
 public class MyEncrypter {
@@ -30,10 +41,17 @@ public class MyEncrypter {
         }
     }
     public static class Aes {
-        private String salt;
-        public Aes(){}
-        public void setSalt(String salt) {
-            this.salt = salt;
+        private static final String CIPHER_INSTANCE = "AES/ECB/PKCS5Padding";
+        public static SecretKeySpec getKey(String clave) 
+            throws UnsupportedEncodingException, NoSuchAlgorithmException 
+        {
+            //byte[] claveEncriptar = clave.getBytes("UTF-8");
+            //MessageDigest sha = MessageDigest.getInstance("SHA-1");
+
+            byte[] claveEncriptar = MessageDigest.getInstance("SHA-1").digest(clave.getBytes("UTF-8"));
+            claveEncriptar = Arrays.copyOf(claveEncriptar, 16);
+
+            return new SecretKeySpec(claveEncriptar, "AES");
         }
     }
 }
