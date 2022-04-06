75ef336abcf55e9f62781975ed168cc021e0cf2d
fbaguem2021 <fbaguem2021@cepnet.net>
2022-04-06 15:50:12 +0200

    Eliminados los constructores de MyEncrypter.BlowFish

--- src/llibreria/encriptation/MyEncrypter.java
+++ src/llibreria/encriptation/MyEncrypter.java
@@ -16,21 +16,13 @@ import org.mindrot.jbcrypt.BCrypt;
 
 public class MyEncrypter {
     public static class BlowFish {
-        private String salt;
-        public BlowFish(){}
-        public void setSalt(String salt){
-            this.salt = salt;
-        }
-        public String getSalt() {
-            return this.salt;
-        }
         public static String genSalt(){
             return BCrypt.gensalt();
         }
-        public static String genSalt(int i) {
+        public String genSalt(int i) {
             return BCrypt.gensalt(i);
         }
-        public static String genSalt(int i, SecureRandom sgr) { 
+        public String genSalt(int i, SecureRandom sgr) { 
             return BCrypt.gensalt(i, sgr);
         }
         public String hashpw(String text, String salt) {
@@ -39,6 +31,7 @@ public class MyEncrypter {
         public boolean checkpw(String text, String hash) {
             return BCrypt.checkpw(text, hash);
         }
+
     }
     public static class Aes {
         private static final String CIPHER_INSTANCE = "AES/ECB/PKCS5Padding";
