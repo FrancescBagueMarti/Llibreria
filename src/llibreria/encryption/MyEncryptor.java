4d4225d7c6a3f069e17cfb7baa177503abb97b3f
fbaguem2021 <fbaguem2021@cepnet.net>
2022-04-12 14:13:50 +0200

    Añadidos metodos en AES

--- src/llibreria/encryption/MyEncryptor.java
+++ src/llibreria/encryption/MyEncryptor.java
@@ -31,20 +31,55 @@ public class MyEncryptor {
         public boolean checkpw(String text, String hash) {
             return BCrypt.checkpw(text, hash);
         }
-
     }
     public static class Aes {
         private static final String CIPHER_INSTANCE = "AES/ECB/PKCS5Padding";
-        public static SecretKeySpec getKey(String clave) 
-            throws UnsupportedEncodingException, NoSuchAlgorithmException 
-        {
-            //byte[] claveEncriptar = clave.getBytes("UTF-8");
-            //MessageDigest sha = MessageDigest.getInstance("SHA-1");
 
+        public SecretKeySpec getKey(String clave) throws UnsupportedEncodingException, NoSuchAlgorithmException {
             byte[] claveEncriptar = MessageDigest.getInstance("SHA-1").digest(clave.getBytes("UTF-8"));
             claveEncriptar = Arrays.copyOf(claveEncriptar, 16);
-
             return new SecretKeySpec(claveEncriptar, "AES");
+        }
+
+        public static String encriptar (String dato, SecretKeySpec key) 
+            throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, 
+            UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException 
+        {
+            Cipher cipher = Cipher.getInstance(CIPHER_INSTANCE);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+
+            byte[] datosAEncriptar = dato.getBytes("UTF-8");
+            byte[] bytesEncriptados = cipher.doFinal(datosAEncriptar);
+
+            return Base64.getEncoder().encodeToString(bytesEncriptados);
+        }
+
+        public static String desencriptar (String dato, SecretKeySpec key) 
+            throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, 
+            IllegalBlockSizeException, BadPaddingException 
+        {
+            Cipher cipher = Cipher.getInstance(CIPHER_INSTANCE);
+            cipher.init(Cipher.DECRYPT_MODE, key);
+
+            byte[] bytesEncriptados = Base64.getDecoder().decode(dato);
+            byte[] datoDesencriptado = cipher.doFinal(bytesEncriptados);
+
+            return new String(datoDesencriptado);
+        }
+
+        public static String generarCadena(String... args) {
+            String cadena = "";
+            for (String s : args) {
+                cadena += s + ":";
+            }
+            return cadena.substring(0, cadena.length()-1);
+        }
+        public static String generarCadena(char separator, String... args) {
+            String cadena = "";
+            for (String s : args) {
+                cadena += s + separator;
+            }
+            return cadena.substring(0, cadena.length()-1);
         }
     }
 }
